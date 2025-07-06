ff6b0ab541542773b57297c68456227d310c2b1f
Dina Adel <dinaadelshehat43@gmail.com>
2025-07-06 20:08:48 +0200

    add floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class NoteApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: NotesView(),
+      home: const NotesView(),
     );
   }
 }
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp/views/widgets/note_item.dart';
 
