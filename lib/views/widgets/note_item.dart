67211263efd271e70a12efa8bb8957e4085465c4
Dina Adel <dinaadelshehat43@gmail.com>
2025-07-07 11:34:21 +0200

    create edit note view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,47 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 135, 255, 255),
-        borderRadius: BorderRadius.circular(17),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 135, 255, 255),
+          borderRadius: BorderRadius.circular(17),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26),
+              ),
+              subtitle: const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'Build your career with Dina Adel',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
                   color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26),
-            ),
-            subtitle: const Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Text(
-                'Build your career with Dina Adel',
-                style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-              ),
+            const Padding(
+              padding: EdgeInsets.only(right: 24),
+              child: Text('Jul6 2025', style: TextStyle(color: Colors.black)),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text('Jul6 2025', style: TextStyle(color: Colors.black)),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
