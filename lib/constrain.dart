83b90d756c8f8ad792df47d9d77eb0674f4a20ee
Dina Adel <dinaadelshehat43@gmail.com>
2025-07-10 00:22:20 +0200

    init hive open notes box

--- lib/constrain.dart
+++ lib/constrain.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const KPrimaryColor = const Color.fromARGB(255, 117, 208, 250);
+const kNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:newsapp/constrain.dart';
 import 'package:newsapp/views/notes_view.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => NoteApp(), // Wrap your app
-      ),
-    );
+// void main() async => runApp(
+//       DevicePreview(
+//         enabled: !kReleaseMode,
+//         builder: (context) => NoteApp(), // Wrap your app
+//       ),
+//     );
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  runApp(NoteApp());
+}
 
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
