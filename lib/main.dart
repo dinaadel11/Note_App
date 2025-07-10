3349e8854381f1e49c0f72d3d140b1f82c09f345
Dina Adel <dinaadelshehat43@gmail.com>
2025-07-10 12:06:16 +0200

    take input and validate data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:hive/hive.dart';
@@ -11,11 +10,27 @@ import 'package:newsapp/views/notes_view.dart';
 //         enabled: !kReleaseMode,
 //         builder: (context) => NoteApp(), // Wrap your app
 //       ),
-//     );
+// //     );
+// void main() async {
+//   await Hive.initFlutter();
+//   await Hive.openBox(kNotesBox);
+//   runApp(NoteApp());
+// }
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
   await Hive.initFlutter();
+
+  // ممكن تفتحي Box هنا لو حابة
   await Hive.openBox(kNotesBox);
-  runApp(NoteApp());
+
+  runApp(
+    DevicePreview(
+      enabled: true, // خليها false لما تخلصي testing
+      builder: (context) => const NoteApp(), // Wrap your app
+    ),
+  );
 }
 
 class NoteApp extends StatelessWidget {
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:newsapp/constrain.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.ontap});
+  final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: KPrimaryColor,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: KPrimaryColor,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
         ),
       ),
     );
